06641145152b27a9a1e1f4a7bd50587e2115ff75
Mayuko <52443198+Mayukoo@users.noreply.github.com>
2021-04-25 17:30:40 +0700

    Add files via upload

---
+++ moneymanagement/lib/screen/launcher.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:moneymanagement/screen/addData.dart';
+import 'package:moneymanagement/screen/home.dart';
+import 'package:moneymanagement/screen/report.dart';
+import 'package:moneymanagement/globals.dart' as globals;
+
+
+
+class Launcher extends StatefulWidget {
+  static const routeName = '/';
+
+
+
+
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LauncherState();
+  }
+
+  void changeTab(var index){
+    _LauncherState()._onItemTapped(index);
+  }
+}
+
+class _LauncherState extends State<Launcher> {
+
+  int _selectedIndex = 0;
+  List<Widget> _pageWidget = <Widget>[
+    Home(),
+    addData(),
+    Report(),
+    Home(),
+    Home(),
+  ];
+  List<BottomNavigationBarItem> _menuBar
+  = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      title: Text('Home'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.business),
+      title: Text('AddData'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.report),
+      title: Text('Report'),
+    ),
+    // BottomNavigationBarItem(
+    //   icon: Icon(Icons.school),
+    //   title: Text('Profile'),
+    // ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pageWidget.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        key: globals.globalKey,
+        items: _menuBar,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme
+            .of(context)
+            .primaryColor,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
